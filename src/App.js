ae117382eb43a42da24538044cf6e38022e9cd3a
Fredy Flores Lemus <fredyfloreslemus@Fredys-MacBook-Pro.local>
2022-09-02 10:07:28 -0600

    [ADD] loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
-import { Col } from "antd";
+import { Col, Spin } from "antd";
 import "./App.css";
 import logo from "./statics/pokeball.png";
 import { useEffect } from "react";
-import { getPokemons, getPokemonDetails } from "./api";
-import { getPokemonsWithDetails } from "./actions";
+import { getPokemons } from "./api";
+import { getPokemonsWithDetails, setLoading } from "./actions";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const pokemons = useSelector((state) => state.pokemons);
+  const { pokemons, loading } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(setLoading(true));
     getPokemons()
       .then((pokemons) => dispatch(getPokemonsWithDetails(pokemons)))
+      .finally(() => dispatch(setLoading(false)))
       .catch((error) => {
         console.error(error);
       });
@@ -31,7 +33,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {loading ? (
+        <Col span={12} offset={12}>
+          <Spin spinning size="large" />
+        </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
     </div>
   );
 }
